5c321ceb2f2f7a0493e4a580801d63a5c6a01d22
Natalie Barba <nbarba2010@gmail.com>
2021-12-19 10:38:43 -0800

    passed all tests

--- ruby/queue.rb
+++ ruby/queue.rb
@@ -11,39 +11,51 @@ class Queue
   # add item to rear of queue if not full
   # if full, throw error
   def enqueue(item)
+    raise "Queue is full" if isFull?
+
+    @queue.push(item)
   end
 
   # remove item from front of queue and return it
   def dequeue
+    @queue.shift
   end
 
   # return item at front of queue without removing it
   def peek
+    # @queue[0]
+    @queue.first
   end
 
   # return true if queue is empty, otherwise false
   def isEmpty?
+    @queue.empty?
   end
 
   # return true if queue is full, otherwise false
   def isFull?
+    @limit && @queue.length == @limit
   end
 
   # return number of items in queue
   def size
+    @queue.length
   end
 
   # return -1 if item not in queue, otherwise integer representing 
   # how far it is from the front
   def search(target)
+    @queue.index(target) || -1
   end
 
   # print contents of queue: do not return the queue itself!
   def print
+    puts @queue.join(", ")
   end
+
 end
 
-if __FILE__ == $PROGRAM_NAME
-  # Don't forget to add your tests!
-end
+# if __FILE__ == $PROGRAM_NAME
+#   # Don't forget to add your tests!
+# end
 
